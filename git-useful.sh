865747ef3cf4ac0a7619965ecb17fb967c8b73e6
杨亚梦 <mz.yyam@gmail.com>
2017-04-06 23:34:50 +0800

    开始收集git常用命令

---
+++ git-useful.sh
@@ -0,0 +1,6 @@
+# 还原到某个版本
+
+# ref: http://stackoverflow.com/questions/4372435/how-can-i-rollback-a-github-repository-to-a-specific-commit
+git reset --hard <old-commit-id>
+# git push -f <remote-name> <branch-name>  # 这句不知道后面两个参数怎么写
+git push origin HEAD --force
