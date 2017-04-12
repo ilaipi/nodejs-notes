5c0e8752338628b47e9a0d23e7b277c994c80710
杨亚梦 <mz.yyam@gmail.com>
2017-04-12 14:21:25 +0800

    Update git-useful.sh

--- git-useful.sh
+++ git-useful.sh
@@ -2,5 +2,5 @@
 
 # ref: http://stackoverflow.com/questions/4372435/how-can-i-rollback-a-github-repository-to-a-specific-commit
 git reset --hard <old-commit-id>
-# git push -f <remote-name> <branch-name>  # 这句不知道后面两个参数怎么写
+# git push -f <remote-name> <branch-name>  # 这句不知道后面两个参数怎么写 用下面一句
 git push origin HEAD --force
